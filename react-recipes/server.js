3c94a474d53faecc07e66225c0cdebe7c4f23d7b
lma <lukas.machetanz@smarter-ecommerce.com>
2018-07-20 09:03:13 +0200

    Finished video 2.8

--- react-recipes/server.js
+++ react-recipes/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+
+// Connects to the database via Mongoose and mLab
 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('Database connected.'))
     .catch(err => console.error(err));
+
+
+
+// Initializes application
 
 const app = express();
 const PORT = process.env.PORT || 4444;
